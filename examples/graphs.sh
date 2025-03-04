1171806bc0974c51697a1522d35117d468c7dacd
Zeyu Zhang <39144422+zeyu2001@users.noreply.github.com>
2025-03-04 23:04:12 +0000

    update examples and example script

--- examples/graphs.sh
+++ examples/graphs.sh
@@ -6,3 +6,7 @@
 c=1; for file in ./*.dot; do dot -Tpng $file -o "$1/$1-module-graph-$c.png"; ((c++)); rm $file; done
 
 ./../circt/build/bin/circt-opt --hw-print-instance-graph "$1/$1.mlir" -o /dev/null 2>&1 | dot -Tpng -o "$1/$1-instance-graph.png";
+
+./../circt/build/bin/circt-opt --hw-print-instance-json "$1/$1.mlir" -o /dev/null --outfile "$1/$1-instance.json";
+
+./../circt/build/bin/circt-opt --hw-print-module-json "$1/$1.mlir" -o /dev/null --outfile "$1/$1-module.json";
--- run_adapter.sh
+++ run_adapter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python3 -m venv venv
-source $PWD/venv/bin/activate
-pip install ai-edge-model-explorer
+source $PWD/.venv/bin/activate
+pip install -e .
 model-explorer --extensions=circt_adapter
