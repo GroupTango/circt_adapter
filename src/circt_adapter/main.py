fd8cfed7f2987d6280a764dced7afd690df025f3
Calin Novo <calin_novo@hotmail.com>
2025-03-04 12:25:42 +0000

    Ran ./format.sh

--- src/circt_adapter/main.py
+++ src/circt_adapter/main.py
@@ -41,27 +41,33 @@ def ConvertCirctToJson(mlir_str: str) -> str:
 
 
 def ConvertJsonToGraphs(json_str: str):
-  file_path = os.path.join(os.path.dirname(__file__) ,'nodeColours.json')
+  file_path = os.path.join(os.path.dirname(__file__), 'nodeColours.json')
   style_dict = {}
   resp = json.loads(json_str)
 
   try:
     with open(file_path, mode='rb') as style_file:
-       style_string = style_file.read()
+      style_string = style_file.read()
     style_dict = json.loads(style_string)
   except FileNotFoundError:
-     return [GraphCollection(label=item['label'], graphs=item['subgraphs']) for item in resp]
+    return [
+        GraphCollection(label=item['label'], graphs=item['subgraphs'])
+        for item in resp
+    ]
 
   for i, ni in enumerate(resp):
     for j, nj in enumerate(ni['subgraphs']):
-      for k, nk in enumerate(nj["nodes"]):
-        if ("attrs" in nk):        
-            for l,nl in enumerate(nk["attrs"]):
-                if (nl["key"] == "type") and (nl["value"] in style_dict):
-                    nk["style"] = style_dict[nl["value"]]
+      for k, nk in enumerate(nj['nodes']):
+        if 'attrs' in nk:
+          for l, nl in enumerate(nk['attrs']):
+            if (nl['key'] == 'type') and (nl['value'] in style_dict):
+              nk['style'] = style_dict[nl['value']]
 
-  return [GraphCollection(label=item['label'], graphs=item['subgraphs']) for item in resp]
-  
+  return [
+      GraphCollection(label=item['label'], graphs=item['subgraphs'])
+      for item in resp
+  ]
+
 
 def CloneModule(src_node: graph_builder.GraphNode) -> graph_builder.GraphNode:
   return None
