b2138177400617a23217eedd487c41eb36ca1218
= <=>
2025-03-11 15:50:27 +0000

    added filepaths to config

--- cypress.config.ts
+++ cypress.config.ts
@@ -8,4 +8,8 @@ export default defineConfig({
   },
   video: true,
   videoCompression: true,
+  // temp: remove when done testing
+  env: {
+    filepaths: ['examples/three/three-module.json']
+  }
 });
